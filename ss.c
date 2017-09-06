a0f7977b019b6052552d0341dd83866e15fef8b8
ajithsimbu <31534541+ajithsimbu@users.noreply.github.com>
2017-09-06 16:18:38 +0530

    Create ss.c

---
+++ ss.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    if(num % 2 == 0)
+        printf("%d is even.", number);
+    else
+        printf("%d is odd.", number);
+    return 0;
+}
